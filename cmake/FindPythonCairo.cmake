fda7d4375883cb18a5f21b6a6b767fe13e9bbc08
David Edmundson <kde@davidedmundson.co.uk>
2019-01-16 14:17:25 +0000

    Check for python-cairo
    Test Plan:
    Works correctly when installed
    
    Changed cmake locally to "import asdfasdf"
    Got the correct error message and an aborted build
    
    Reviewers: #plasma, ngraham, cgiboudeaux, zzag
    
    Reviewed By: #plasma, ngraham, zzag
    
    Subscribers: ngraham, zzag, plasma-devel
    
    Tags: #plasma
    
    Differential Revision: https://phabricator.kde.org/D18244

---
+++ cmake/FindPythonCairo.cmake
@@ -0,0 +1,14 @@
+if(CMAKE_VERSION VERSION_LESS 3.12.0)
+    find_package(PythonInterp 3 REQUIRED)
+    set(PYTHON_EXECUTABLE "${Python3_EXECUTABLE}")
+else()
+    find_package(Python3 COMPONENTS Interpreter REQUIRED)
+    set(PYTHON_EXECUTABLE "${Python3_EXECUTABLE}")
+endif()
+
+# Check for python cairo
+execute_process(COMMAND ${PYTHON_EXECUTABLE} -c "import cairo"
+                RESULT_VARIABLE PYTHON_CAIRO_RESULT)
+if (PYTHON_CAIRO_RESULT EQUAL 0)
+    set(PYTHONCAIRO_FOUND 1)
+endif()
