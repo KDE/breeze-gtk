51e27ecf40574d945fc24845877c3780e7a693bb
Harald Sitter <sitter@kde.org>
2019-01-17 08:43:37 +0100

    don't set python exe to undefined
    Summary:
    FindPythonInterp sets PYTHON_EXECUTABLE, we overrode it by setting an
    empty var. add a comment to explain the unbalancedness of the conditional
    branches.
    
    Test Plan: pythoncairo is correctly found with cmake 3.10.2
    
    Reviewers: davidedmundson, fvogt
    
    Reviewed By: fvogt
    
    Subscribers: fvogt, heikobecker, plasma-devel
    
    Tags: #plasma
    
    Differential Revision: https://phabricator.kde.org/D18309

--- cmake/FindPythonCairo.cmake
+++ cmake/FindPythonCairo.cmake
@@ -1,6 +1,6 @@
 if(CMAKE_VERSION VERSION_LESS 3.12.0)
     find_package(PythonInterp 3 REQUIRED)
-    set(PYTHON_EXECUTABLE "${Python3_EXECUTABLE}")
+    # PythonInterp sets PYTHON_EXECUTABLE
 else()
     find_package(Python3 COMPONENTS Interpreter REQUIRED)
     set(PYTHON_EXECUTABLE "${Python3_EXECUTABLE}")
