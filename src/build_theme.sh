fdd2df48b37eee31799262b09ce001942e968f4c
Carson Black <uhhadd@gmail.com>
2019-08-02 10:36:16 -0600

    Merge gtk320 and gtk318 into gtk3
    Summary:
    GTK 3.20 and GTK 3.18 are now merged into a single GTK 3.0 directory. This will reduce work needed in the future for maintenance and updates.
    
    In the past, GTK3 had an extremely unstable theming API, however, as GTK4 draws near, less focus has been given to breaking existing CSS with GTK3. This makes serving specific versions of GTK CSS redundant and doubles the work required to make changes for gains most users aren't likely to see.
    
    Test Plan: See that themes work properly.
    
    Reviewers: #plasma, ngraham
    
    Reviewed By: ngraham
    
    Subscribers: ngraham, plasma-devel
    
    Tags: #plasma
    
    Differential Revision: https://phabricator.kde.org/D22876

--- src/build_theme.sh
+++ src/build_theme.sh
@@ -3,7 +3,7 @@ set -e
 
 # Usage: create_folders <target-directory>
 create_folders () {
-  for j in gtk-2.0 gtk-3.18 gtk-3.20; do
+  for j in gtk-2.0 gtk-3.0; do
     if ! [ -d "$1/$j" ]; then
         mkdir -p "$1/$j"
     fi
@@ -28,7 +28,7 @@ install_theme () {
   fi
   echo "Installing into ${THEME_INSTALL_TARGET}"
   mkdir -p "${THEME_INSTALL_TARGET}"
-  for dir in assets gtk-2.0 gtk-3.18 gtk-3.20; do
+  for dir in assets gtk-2.0 gtk-3.0; do
     if [ -d "${THEME_INSTALL_TARGET}/$dir" ]; then
       rm -rf "${THEME_INSTALL_TARGET:?}/$dir"
     fi
@@ -44,8 +44,7 @@ render_theme () {
   cp -R gtk2/* "${THEME_BUILD_DIR}/gtk-2.0/"
   python3 render_assets.py -c "$1" -a "${THEME_BUILD_DIR}/assets" \
     -g "${THEME_BUILD_DIR}/gtk-2.0" -G "${THEME_BUILD_DIR}" -b "$4"
-  build_sass gtk318/gtk.scss "${THEME_BUILD_DIR}/gtk-3.18/gtk.css" "${THEME_BUILD_DIR}"
-  build_sass gtk320/gtk.scss "${THEME_BUILD_DIR}/gtk-3.20/gtk.css" "${THEME_BUILD_DIR}"
+  build_sass gtk3/gtk.scss "${THEME_BUILD_DIR}/gtk-3.0/gtk.css" "${THEME_BUILD_DIR}"
   rm -f "${THEME_BUILD_DIR}/_global.scss"
   install_theme "${THEME_BUILD_DIR}" "$2" "$3"
 }
